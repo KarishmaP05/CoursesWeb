35278c9676dca68e9807176f9b53d7135adefcb6
Chetan Rathod <chetan0101career@gmail.com>
2024-01-13 17:22:29 +0530

    use sqlite db

--- CoursesWeb/settings.py
+++ CoursesWeb/settings.py
@@ -75,12 +75,8 @@ WSGI_APPLICATION = 'CoursesWeb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': "CoursesDB",
-        'HOST': "localhost",
-        'USER': "postgres",
-        'PASSWORD': "admin",
-        'PORT':"5432"
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
