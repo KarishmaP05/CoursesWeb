f48a0e207604bec365318040ce070c507f38842c
KarishmaP05 <phapalekarishma80@gmail.com>
2024-02-03 13:21:00 +0530

    extended all templates from base

--- CoursePortal/urls.py
+++ CoursePortal/urls.py
@@ -5,5 +5,11 @@ from . import views
 app_name = 'CoursePortal'
 
 urlpatterns = [
-    path('',views.index, name="index" )
+    path('',views.index, name="index" ),
+    path('about/',views.about,name="aboutus" ),
+    path('courses/',views.courses,name="courses" ),
+    path('contact/',views.contact,name="contact" ),
+    path('team/',views.team,name="team" ),
+    path('testimonial/',views.testimonial,name="testimonial" ),
+    path('error404/',views.error404,name="error404" )
 ]
