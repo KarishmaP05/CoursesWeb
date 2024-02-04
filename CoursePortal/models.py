5122fae6e4a70e6bd19dbcc42fe1ba75adaf0a63
Chetan Rathod <chetan0101career@gmail.com>
2024-02-04 14:20:52 +0530

    models for contact

--- CoursePortal/models.py
+++ CoursePortal/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=100)
+    subject = models.CharField(max_length=300)
+    date = models.DateField(default=date.today())
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
